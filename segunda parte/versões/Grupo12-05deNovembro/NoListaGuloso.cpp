331e9afef9d25bd8549571a6907482e48f5c018e
nietz9 <42752884+nietz9@users.noreply.github.com>
2019-11-05 17:41:40 -0300

    Add files via upload

---
+++ segunda parte/versões/Grupo12-05deNovembro/NoListaGuloso.cpp
@@ -0,0 +1,34 @@
+#include "NoListaGuloso.h"
+
+NoListaGuloso::NoListaGuloso(No* id)
+{
+    ponteiro = id;
+}
+
+NoListaGuloso::~NoListaGuloso()
+{
+    if(proximo != nullptr)
+        proximo.setAnt(this.anterior);
+    if(anterior != nullptr)
+        anterior.setProx(this.proximo);
+}
+
+void NoListaGuloso::setProx(NoListaGuloso* prox)
+{
+    this.proximo = prox;
+}
+
+void NoListaGuloso::setAnt(NoListaGuloso* ant)
+{
+    this.anterior = ant;
+}
+
+NoListaGuloso NoListaGuloso::getProx()
+{
+    return proximo;
+}
+
+NoListaGuloso NoListaGuloso::getAnt()
+{
+    return anterior;
+}
